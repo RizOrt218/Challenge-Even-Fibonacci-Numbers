65a4a32d7a33af7dee5833bedf020862c3a67002
RizOrt218 <rizhiafalen@gmail.com>
2015-12-08 10:38:36 -1000

    need to update

--- even_fib_nums.js
+++ even_fib_nums.js
@@ -12,8 +12,30 @@ module.exports = {
 
 function _sumFibs( maxFibValue ) {
   var sum = 0;
+  var firstNum = 0;
+  var secondNum = 1;
+  var nextNum = 1;
 
-  // do your work here
+  if(maxFibValue < 2){
+    return 0;
+  }
+
+  for(var i = 2; i <= maxFibValue; i++ ){
+    nextNum = firstNum + secondNum;
+    firstNum = secondNum;
+    secondNum = nextNum;
+
+  if(nextNum % 2 ===0){
+    sum += nextNum;
+  }
+
+  }
+  return nextNum;
+
+
+
+
+
 
   return sum;
 }
@@ -28,4 +50,4 @@ function highestFibonacciNumber (maxFibValue){
   //do your work here
 
   return highest;
-};
+}
